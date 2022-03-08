92ad4466b4691e4da04bd8b7ac03f2d2427b0cb0
Bhagirathi <bhagirathi.nayak@hotmail.com>
2022-03-08 17:12:33 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './custom/home-page/home-page.component';
+import { HeaderComponent } from './custom/header/header.component';
+import { NavBarComponent } from './custom/nav-bar/nav-bar.component';
+import { ContentAreaComponent } from './custom/content-area/content-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    NavBarComponent,
+    ContentAreaComponent
   ],
   imports: [
     BrowserModule,
